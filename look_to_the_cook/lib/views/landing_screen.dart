ac638a26b5d1e617c3ee2da158ed3c6aa5c99e54
shaynaBiohacked <shayna.jamieson@biohacked.com>
2020-01-30 11:38:08 -0800

    Add splash screen basic UI

---
+++ look_to_the_cook/lib/views/landing_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:look_to_the_cook/templates/background_container_image.dart';
+import 'package:look_to_the_cook/templates/rounded_button.dart';
+
+class LandingScreen extends StatelessWidget {
+  static const String id = 'landing_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BackgroundContainerImage(
+        image: 'images/home_screen_bg.png',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(child: SizedBox(),),
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+            child: RoundedButton(
+              title: 'LOG IN',
+              buttonColor: Colors.white,
+              buttonTextColor: Colors.black,
+              onPressed: () {
+
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0, right: 18.0, bottom: 50.0, top: 10.0),
+            child: RoundedButton(
+              title: 'REGISTER',
+              buttonColor: Colors.white,
+              buttonTextColor: Colors.black,
+              onPressed: () {
+                
+              },
+            ),
+          ),
+        ],
+        ),
+      ),
+    );
+  }
+}

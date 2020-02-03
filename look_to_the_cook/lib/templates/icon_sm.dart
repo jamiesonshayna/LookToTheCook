3322aa7a172dfda732124c945f510a6490ab65eb
Robert Wood Jr <rdrwood@gmail.com>
2020-02-02 20:12:35 -0800

    added comments

--- look_to_the_cook/lib/templates/icon_sm.dart
+++ look_to_the_cook/lib/templates/icon_sm.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'package:url_launcher/url_launcher.dart';
+
+
+/*
+Authors: Shayna Jamieson, Rob Wood
+Date Created: 02/01/2020
+Last Modified: 02/02/2020
+File Name: icon_sm.dart
+Version: 2.0
+Description: The purpose of this file is to build a widget that based on the
+info sent build a social media icon and link to te proper social media
+corresponding to the user and the one chose.
+ */
+
 class IconSM extends StatelessWidget {
   final String link;
   final IconData whichIcon;
   IconSM(
-    {
+    { // requires to know what media icon you want & the address of the link
+      // that it will take you to.
       @required this.link,
       @required this.whichIcon,
     });
@@ -14,16 +28,15 @@ class IconSM extends StatelessWidget {
       return new IconButton(
         // Use the FontAwesomeIcons class for the IconData
           icon: new Icon(whichIcon),
+          color: Colors.red,
           onPressed: () {
+            // when pressed will launch the url supplied to the link
             _launchURL(link);
           }
       );
   }
+  // this is used to launch the links to the social media platforms
   _launchURL(String url) async {
     launch(url);
   }
 }
-
-
-//   'https://www.linkedin.com/in/sourabh-kumar-08987560/'),
-//
--- look_to_the_cook/lib/views/aboutus_screen.dart
+++ look_to_the_cook/lib/views/aboutus_screen.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+
 // TEMPLATE COMPONENTS:
 import 'package:look_to_the_cook/templates/app_bar_component.dart';
 import 'package:look_to_the_cook/templates/icon_sm.dart';
 import 'package:look_to_the_cook/templates/normal_text.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
+
 // ROUTES
+
 
 /*
 Authors: Shayna Jamieson, Rob Wood
 Date Created: 01/30/2020
-Last Modified: 02/01/2020
+Last Modified: 02/02/2020
 File Name: aboutus_screen.dart
 Version: 2.0
 Description: The purpose of this file is to build and render the about us screen.
-The screen..........
+The screen will add a photo followed by the developer's name, then below that \
+in another row include github linkedin and email links, Followed by a
+brief statement bout the developer.
  */
 
 class AboutUsScreen extends StatelessWidget {
@@ -35,18 +38,19 @@ class AboutUsScreen extends StatelessWidget {
           },
         ),
       ),
+
       body: Column(
           children: <Widget>[
             SizedBox(
               height: 20.0,
-            ),
+            ), // to space between the appbar and text
             NormalText(
-              text: "The Developers",
-              textSize: 18,
+              text: "Meet The Team",
+              textSize: 24,
               textColor : Colors.black,
             ),
             SizedBox(
-              height: 20.0,
+              height: 30.0,
               width: 200.0,
               child: Divider(
                 color: Colors.black,
@@ -64,14 +68,12 @@ class AboutUsScreen extends StatelessWidget {
             Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-
                 IconSM(
                   link: 'https://github.com/jamiesonshayna',
                   whichIcon: FontAwesomeIcons.github,
                 ),
-
                 IconSM(
-                    link: 'mailto:sjamieson2@mail.greenriver.edu',
+                    link: 'mailto:jamieson.shayna@gmail.com',
                     whichIcon: FontAwesomeIcons.solidEnvelope,
                 ),
                   IconSM(
@@ -81,8 +83,8 @@ class AboutUsScreen extends StatelessWidget {
 
         ]),
             NormalText(
-              text: "   test to add ",
-              textSize: 14,
+              text: "Software Developer -- Leveraging cultivated knowledge && an eagerness to absorb new tricks/technologies.",
+              textSize: 16,
               textColor : Colors.black,
             ),
 
@@ -95,19 +97,17 @@ class AboutUsScreen extends StatelessWidget {
               backgroundImage: AssetImage('images/rw_prof.jpg'),
             ),
             NormalText(
-              text: "Rob Wood",
-              textSize: 14,
+              text: "Robert Wood Jr",
+              textSize: 16,
               textColor : Colors.black,
             ),
             Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-
                   IconSM(
                     link: 'https://github.com/jamiesonshayna',
                     whichIcon: FontAwesomeIcons.github,
                   ),
-
                   IconSM(
                     link: 'mailto:rdrwood@gmail.com',
                     whichIcon: FontAwesomeIcons.solidEnvelope,
@@ -116,21 +116,16 @@ class AboutUsScreen extends StatelessWidget {
                     link: 'https://www.linkedin.com/in/robert-wood-jr-rdk/',
                     whichIcon: FontAwesomeIcons.linkedin,
                   ),
-
                 ]),
             NormalText(
-              text: "Software development student currently in my junior year, "
-                  "preparing to embark on the tech industry as a software "
-                  "developer.",
+              text: "Software developer embarking into the tech industry "
+                  "to make a difference in lives.",
               textSize: 16,
               textColor : Colors.black,
             ),
           ]
       )
     );
-  }
-  _launchURL(String url) async {
-    launch(url);
   }
 }
 

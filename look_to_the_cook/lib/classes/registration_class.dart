41a81cde851184d9bf83ac18514e27ab700b4e6e
shaynaBiohacked <shayna.jamieson@biohacked.com>
2020-02-19 14:36:13 -0800

    Get aws user pool id and client id

---
+++ look_to_the_cook/lib/classes/registration_class.dart
@@ -0,0 +1,27 @@
+import 'package:amazon_cognito_identity_dart/cognito.dart';
+import 'package:amazon_cognito_identity_dart/sig_v4.dart';
+
+/*
+POOL NAME: LookToTheCookUsers
+APP CLIENT NAME: LookToTheCookClient
+APP CLIENT ID: 1hhicps9mo7ukqgqkvq9nnp36n
+USER POOL ID: us-west-2_dscLXwSqb
+
+ */
+
+/*
+Authors: Shayna Jamieson, Rob Wood
+Date Created: 02/19/2020
+Last Modified: 02/19/2020
+File Name: registration_class.dart
+Version: 1.0
+Description: This.......
+ */
+class Registration {
+  // fields
+  String _clientId = "1hhicps9mo7ukqgqkvq9nnp36n";
+  String _poolId = "us-west-2_dscLXwSqb";
+
+
+
+}

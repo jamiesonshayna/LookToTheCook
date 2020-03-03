46e87dfa00247c12d61066010bf340a742cb4434
shaynaBiohacked <shayna.jamieson@biohacked.com>
2020-03-02 18:38:15 -0800

    Add loading spinner to multiple screens

--- look_to_the_cook/lib/views/landing_screen.dart
+++ look_to_the_cook/lib/views/landing_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:look_to_the_cook/classes/login_logout_class.dart';
 import 'package:look_to_the_cook/classes/secure_storage_class.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:async';
 
 // TEMPLATE COMPONENTS:
 import 'package:look_to_the_cook/templates/constants.dart';
@@ -15,74 +18,103 @@ import 'package:look_to_the_cook/views/home_screen.dart';
 /*
 Authors: Shayna Jamieson, Rob Wood
 Date Created: 01/30/2020
-Last Modified: 02/19/2020
+Last Modified: 03/02/2020
 File Name: landing_screen.dart
-Version: 3.0
+Version: 4.0
 Description: The purpose of this file is to build and render the landing screen.
 This screen is the initial route for the Look To The Cook application. This is a static
 screen that simply allows users to choose between logging in, auto logging in,
  or creating a profile.
  */
 
-class LandingScreen extends StatelessWidget {
+class LandingScreen extends StatefulWidget {
   static const String id = 'landing_screen';
+
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  // variable to track loading spinner and toggle
+  bool _isLoggingIn = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: BackgroundContainerImage(
         image: 'images/new_home_screen.png', // set body background image
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(child: SizedBox(),),
-          Padding(
-            padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-            child: RoundedButton(
-              title: 'LOG IN',
-              buttonColor: kRedButtonColor,
-              buttonTextColor: Colors.white,
-              onPressed: () async {
-                // check if we can automatically move a logged in user to the home screen
-                SecureStorage storage = new SecureStorage();
-                if(await storage.userIsLoggedIn()) {
-                  // attempt to authenticate user and navigate them to the home screen
-                  LoginLogout loginHelper = new LoginLogout();
+        child: ModalProgressHUD(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(child: SizedBox(),),
+            Padding(
+              padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+              child: RoundedButton(
+                title: 'LOG IN',
+                buttonColor: kRedButtonColor,
+                buttonTextColor: Colors.white,
+                onPressed: () async {
+                  // check if we can automatically move a logged in user to the home screen
+                  SecureStorage storage = new SecureStorage();
+                  if(await storage.userIsLoggedIn()) {
+                    setState(() {
+                      _isLoggingIn = true;
 
-                  if(await loginHelper.autoAuthenticateAndLogin()) {
-                    // we have authenticated and can navigate
-                    Navigator.pushNamed(context, HomeScreen.id);
-                  } else { // failed auto-login (needs manual login)
+                    });
+
+                    // let loading spinner go
+                    await new Future.delayed(const Duration(seconds: 1));
+
+                    // attempt to authenticate user and navigate them to the home screen
+                    LoginLogout loginHelper = new LoginLogout();
+
+                    if(await loginHelper.autoAuthenticateAndLogin()) {
+                      setState(() {
+                        _isLoggingIn = false;
+                      });
+                      // we have authenticated and can navigate
+                      Navigator.pushNamed(context, HomeScreen.id);
+                    } else { // failed auto-login (needs manual login)
+                      setState(() {
+                        _isLoggingIn = false;
+                      });
+                      // takes the user to the login_screen
+                      Navigator.pushNamed(context, LoginScreen.id);
+                    }
+                  } else {
                     // takes the user to the login_screen
                     Navigator.pushNamed(context, LoginScreen.id);
                   }
-                } else {
-                  // takes the user to the login_screen
-                  Navigator.pushNamed(context, LoginScreen.id);
-                }
-              },
+                },
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 18.0, right: 18.0, bottom: 50.0, top: 10.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: RoundedButton(
-                    title: 'REGISTER',
-                    buttonColor: kRedButtonColor,
-                    buttonTextColor: Colors.white,
-                    onPressed: () {
-                      // takes the user to the registration_screen
-                      Navigator.pushNamed(context, RegistrationScreen.id);
-                    },
+            Padding(
+              padding: const EdgeInsets.only(left: 18.0, right: 18.0, bottom: 50.0, top: 10.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RoundedButton(
+                      title: 'REGISTER',
+                      buttonColor: kRedButtonColor,
+                      buttonTextColor: Colors.white,
+                      onPressed: () {
+                        // takes the user to the registration_screen
+                        Navigator.pushNamed(context, RegistrationScreen.id);
+                      },
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
+          ],
           ),
-        ],
+          inAsyncCall: _isLoggingIn,
+          dismissible: false,
+          progressIndicator: SpinKitWave(
+            color: kRedButtonColor,
+          )
         ),
       ),
     );
--- look_to_the_cook/lib/views/login_screen.dart
+++ look_to_the_cook/lib/views/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:look_to_the_cook/classes/login_logout_class.dart';
 import 'package:look_to_the_cook/classes/regex_helper_class.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:async';
 
 // TEMPLATE COMPONENTS:
 import 'package:look_to_the_cook/templates/app_bar_component.dart';
@@ -15,9 +18,9 @@ import 'package:look_to_the_cook/views/forgot_password_screen.dart';
 /*
 Authors: Shayna Jamieson, Rob Wood
 Date Created: 02/01/2020
-Last Modified: 02/19/2020
+Last Modified: 03/02/2020
 File Name: login_screen.dart
-Version: 3.0
+Version: 4.0
 Description: The purpose of this file is to allow currently registered users to log into
 their Look To The Cook accounts. Users will use the email and password that they signed up
 with to let the app communicate with AWS and start a valid session. Regex is used again to
@@ -35,6 +38,9 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   // listener for the text field user input that allow us to capture user data
   final formKey = GlobalKey<FormState>();
+
+  // to keep track of the loading spinner
+  bool _isLoggingIn = false;
 
   // regex helper object for validation
   RegexHelperClass regexHelper = new RegexHelperClass();
@@ -62,148 +68,166 @@ class _LoginScreenState extends State<LoginScreen> {
           },
         ),
       ),
-      body: new GestureDetector(
-        onTap: () {
-          // lets the user exit text field by tapping background scaffold
-          FocusScope.of(context).requestFocus(new FocusNode());
-        },
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 100.0, left: 10.0, right: 30.0),
-                child: Container(
-                  child: TextFormField(
-                    // validation for email field on login form
-                    validator: (value) {
-                      if(regexHelper.validateEmail(value.trim()) == false) {
-                        return 'invalid email';
-                      } else {
-                        setState(() {
-                          email = value;
-                        });
-                        return null;
-                      }
-                    },
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                    decoration: InputDecoration(
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: kRedButtonColor),
+      body: ModalProgressHUD(
+        child: GestureDetector(
+          onTap: () {
+            // lets the user exit text field by tapping background scaffold
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 100.0, left: 10.0, right: 30.0),
+                  child: Container(
+                    child: TextFormField(
+                      // validation for email field on login form
+                      validator: (value) {
+                        if(regexHelper.validateEmail(value.trim()) == false) {
+                          return 'invalid email';
+                        } else {
+                          setState(() {
+                            email = value;
+                          });
+                          return null;
+                        }
+                      },
+                      style: TextStyle(
+                        color: Colors.black,
                       ),
-                      icon: Padding(
-                        padding: const EdgeInsets.only(top:10.0),
-                        child: Icon(
-                          Icons.mail_outline,
-                          color: Colors.black,
-                          size: 40.0,
-                        ),
-                      ),
-                      hintText: '',
-                      labelText: 'EMAIL',
-                      labelStyle: TextStyle(
-                        color: kRedButtonColor,
-                        fontSize: 18.0,
-                      )
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0, left: 10.0, right: 30.0),
-                child: Container(
-                  child: TextFormField(
-                    obscureText: true,
-                    // validation for password field on login form
-                    validator: (value) {
-                      if(regexHelper.validatePassword(value) == false) {
-                        return 'invalid password';
-                      } else {
-                        setState(() {
-                          password = value;
-                        });
-                        return null;
-                      }
-                    },
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                    decoration: InputDecoration(
+                      decoration: InputDecoration(
                         focusedBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: kRedButtonColor),
                         ),
                         icon: Padding(
                           padding: const EdgeInsets.only(top:10.0),
                           child: Icon(
-                            Icons.lock_outline,
+                            Icons.mail_outline,
                             color: Colors.black,
                             size: 40.0,
                           ),
                         ),
                         hintText: '',
-                        labelText: 'PASSWORD',
+                        labelText: 'EMAIL',
                         labelStyle: TextStyle(
                           color: kRedButtonColor,
                           fontSize: 18.0,
                         )
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Expanded(child: SizedBox()),
-              Padding(
-                padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: RoundedButton(
-                        title: 'LOG IN',
-                        buttonTextColor: Colors.white,
-                        buttonColor: kRedButtonColor,
-                        onPressed: () async {
-                          // if the user input is valid then we start AWS login
-                          if(formKey.currentState.validate()) {
-                            if(await loginHelper.manualAuthenticateAndLogin(email, password)) {
-                              // have authenticated and set credentials (can log in)
-                              Navigator.pushNamed(context, HomeScreen.id);
-                            } else {
-                              setState(() {
-                                invalidLogin = true;
-                              });
-                            }
-                          }
-                        },
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, left: 10.0, right: 30.0),
+                  child: Container(
+                    child: TextFormField(
+                      obscureText: true,
+                      // validation for password field on login form
+                      validator: (value) {
+                        if(regexHelper.validatePassword(value) == false) {
+                          return 'invalid password';
+                        } else {
+                          setState(() {
+                            password = value;
+                          });
+                          return null;
+                        }
+                      },
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: kRedButtonColor),
+                          ),
+                          icon: Padding(
+                            padding: const EdgeInsets.only(top:10.0),
+                            child: Icon(
+                              Icons.lock_outline,
+                              color: Colors.black,
+                              size: 40.0,
+                            ),
+                          ),
+                          hintText: '',
+                          labelText: 'PASSWORD',
+                          labelStyle: TextStyle(
+                            color: kRedButtonColor,
+                            fontSize: 18.0,
+                          )
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 100.0, top: 15.0),
-                child: SizedBox(
-                    child:
-                    invalidLogin == false ? Text('') : new GestureDetector(
-                      onTap:() {
-                        Navigator.pushNamed(context, ForgotPasswordScreen.id);
-                      }, child: Text.rich(
-                      TextSpan(
-                        text: 'Invalid email or password.  ',
-                        children: <TextSpan>[
-                          TextSpan(
-                              text: 'Forgot Password?',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                              )),
-                          // can add more TextSpans here...
-                        ],
+                Expanded(child: SizedBox()),
+                Padding(
+                  padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: RoundedButton(
+                          title: 'LOG IN',
+                          buttonTextColor: Colors.white,
+                          buttonColor: kRedButtonColor,
+                          onPressed: () async {
+                            setState(() { // start loading spinner
+                              _isLoggingIn = true;
+                            });
+
+                            // let loading spinner go
+                            await new Future.delayed(const Duration(seconds: 1));
+
+                            // if the user input is valid then we start AWS login
+                            if(formKey.currentState.validate()) {
+                              if(await loginHelper.manualAuthenticateAndLogin(email, password)) {
+                                setState(() {
+                                  _isLoggingIn = false;
+                                });
+                                // have authenticated and set credentials (can log in)
+                                Navigator.pushNamed(context, HomeScreen.id);
+                              } else {
+                                setState(() {
+                                  _isLoggingIn = false;
+                                  invalidLogin = true;
+                                });
+                              }
+                            }
+                          },
+                        ),
                       ),
-                    ),)
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 100.0, top: 15.0),
+                  child: SizedBox(
+                      child:
+                      invalidLogin == false ? Text('') : new GestureDetector(
+                        onTap:() {
+                          Navigator.pushNamed(context, ForgotPasswordScreen.id);
+                        }, child: Text.rich(
+                        TextSpan(
+                          text: 'Invalid email or password.  ',
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: 'Forgot Password?',
+                                style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                )),
+                            // can add more TextSpans here...
+                          ],
+                        ),
+                      ),)
+                  ),
+                ),
+              ],
+            ),
           ),
+        ),
+        inAsyncCall: _isLoggingIn,
+        dismissible: false,
+        progressIndicator: SpinKitWave(
+          color: kRedButtonColor,
         ),
       ),
     );
